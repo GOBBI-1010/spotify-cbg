58876479154417fe31c73dbc2940b72aa52a530b
GOBBI-1010 <dgobi20022gmail.com>
2025-03-27 17:58:10 +0530

    fix

--- spotify/vite.config.js
+++ spotify/vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
-    base: process.env.VITE_BASE_PATH ||"/spotify-cbg",
     server: {
       host: '0.0.0.0', // Allows access from other devices on your network
       port: 3000, // You can change this to another port if needed
